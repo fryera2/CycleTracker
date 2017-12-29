cb1b40484cff13ded0c302c7efc86770e9b02b27
Andrew Fryer <andrew_fryer@hotmail.co.uk>
2017-12-29 09:05:35 +0000

    Can create rides and bikes plus simple front-end

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -1,4 +1,21 @@
 class RidesController < ApplicationController
   def new
+    @bikeList=Bike.all
+  end
+
+  def create
+    @ride = Ride.new (ride_params)
+    @ride.save
+
+    redirect_to @ride
+  end
+
+  def show
+
+  end
+
+  private
+  def ride_params
+    params.require(:Ride).permit(:BikeID, :RideDate, :TimeInMinutes, :TimeInHours, :DistanceInMiles, :DistanceInKm, :Ascent)
   end
 end
